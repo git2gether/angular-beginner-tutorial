5eef72d2594ed4b91332ab23995eb2869136503a
git2gether <genelam@gmail.com>
2020-04-21 01:45:08 -0600

    4 files modified, 4 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,6 +13,7 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
   ) { }
 
   ngOnInit() {
@@ -19,5 +21,8 @@ export class ProductDetailsComponent implements OnInit {
       this.product = products[+params.get('productId')];
     });
   }
-
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
 }
